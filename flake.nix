8b8300282e725deab3d996ec4cc00a40862e08a0
t3rro <t3rro@protonmail.com>
2023-02-22 11:29:56 -0800

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "pure functions";
+
+  outputs = {}:
+    let
+      # input is a set of attrs and it is returned in array format
+      # but deep copies all values.
+      attrsToArr = attrs:
+        let
+          addAttr = attrName: result: result ++ [{ name = attrName; value = attrs.${attrName}; }];
+          allAttrs = builtins.attrNames attrs;
+        in
+        builtins.foldl addAttr [ ] allAttrs;
+    in
+    {
+      # use lib keyword on outputs to expose nix functions
+      lib = {
+        inherit attrsToArr;
+      };
+    };
+}
